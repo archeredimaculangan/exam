d2c29faf8b66a8a50ec67974acc2512f219ea0bc
archeredimaculangan <archeresoliven@gmail.com>
2023-02-05 21:28:42 +0800

    landing page

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -13,9 +13,11 @@
         <link rel="dns-prefetch" href="//fonts.gstatic.com">
         <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
         <link href="{{ asset('css/landing.css') }}" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
         <!-- Scripts -->
         <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
         @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     </head>
     <body>
@@ -38,12 +40,12 @@
                         </ul>
 
                         <!-- Right Side Of Navbar -->
-                        <ul class="navbar-nav ms-auto">
+                        <ul class="navbar-nav ">
                             <li class="nav-item">
-                                <a class="btn btn-warning" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="btn btn-warning" href="#">{{ __('Login') }}</a>
                             </li>
                             <li class="nav-item">
-                                <a class="btn btn-warning" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="btn btn-warning" href="#}}">{{ __('Daftar Sekarang') }}</a>
                             </li>
                         </ul>
                     </div>
@@ -75,24 +77,22 @@
                     @endforeach
                     
                 </div>
-                <a class="carousel-control-prev" href="#banner" role="button" data-slide="prev">
+                <a class="carousel-control-prev" href="#banner" role="button" data-slide="prev" onclick="$('#banner').carousel('prev')">
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
                 </a>
-                <a class="carousel-control-next" href="#banner" role="button" data-slide="next">
+                <a class="carousel-control-next" href="#banner" role="button" data-slide="next" onclick="$('#banner').carousel('next')">
                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
                 </a>
             </div>
             <div class="images mt-10">
                 <div class="row justify-content-center">
-                    <div class="col-3">
+                    <div class="col-3 col-sm-3">
                         <img src="{{url('/images/img-cta-3.jpg')}}" alt="">
                     </div>
-                    <div class="col-3">
+                    <div class="col-3 col-sm-3">
                         <img src="{{url('/images/img-cta-2.jpg')}}" alt="">
                     </div>
-                    <div class="col-3">
+                    <div class="col-3 col-sm-3">
                         <img src="{{url('/images/img-cta-1.jpg')}}" alt="">
                     </div>
                 </div>
@@ -116,9 +116,80 @@
                     </ul>
                 </div>
             </div>
-            <main class="py-4">
-                @yield('content')
-            </main>
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-3 text-center first">
+                        <img src="{{url('/images/img-group.svg')}}" alt="Group Icon">
+                        <h2>Tim Pembayaran<br>yang Profesional</h2>
+                        <p>
+                            Metode pembayaran beragam dan terpercaya. Setiap dana pemain aman dan terjamin.
+                        </p>
+                    </div>
+                    <div class="col-3 text-center">
+                        <img src="{{url('/images/img-domino.svg')}}" alt="Group Icon">
+                        <h2>Brand yang<br>Terkenal</h2>
+                        <p>
+                            Anggaran pemasaran yang luar biasa. Biarkan Anda menikmati efek dari merek berkualitas.
+                        </p>
+                    </div>
+                    <div class="col-3 text-center">
+                        <img src="{{url('/images/img-document.svg')}}" alt="Group Icon">
+                        <h2>Sistem Laporan<br>Profesional</h2>
+                        <p>
+                            Format pelaporan yang rapi & bersih. Lihat laporan harian & bulanan di satu titik 
+                        </p>
+                    </div>
+                    <div class="col-3 text-center">
+                        <img src="{{url('/images/img-settings.svg')}}" alt="Group Icon">
+                        <h2>Personalisasikan<br>Paket Pribadi</h2>
+                        <p>
+                            Kami dapat menyesuaikan promosi khusus untuk Anda. Membuat promosi Anda menjadi Mudah 
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <img src="{{url('/images/Sticker WhatsApp Gratis - Kongsi OLE777.gif')}}" alt="ad">
+                </div>
+            </div>
+            <div class="contacts">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-4">
+                            <img src="{{url('/images/icon-skype.svg')}}" alt="skype">
+                            <span  class="white">Pelayanan Pelanggan Skype: <span class="gold">affiliasi.id@oleintl.com</span></span>
+                        </div>
+                        <div class="col-4">
+                            <img src="{{url('/images/icon-email.svg')}}" alt="mail">
+                            <span class="white">Email: <span class="gold">affiliatebola@ole777-indo.com</span></span>
+                        </div>
+                        <div class="col-4">
+                            <img src="{{url('/images/icon-whatsapp.svg')}}" alt="whatsapp">
+                            <span class="white">Whatsapp: <span class="gold">+6287807264445</span></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="footer">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="{{url('/images/logo-chelsea.svg')}}" alt="chelsea">
+                        </div>
+                        <div class="col-3">
+                            <img src="{{url('/images/logo-pagcor.png')}}" alt="pagcor">
+                        </div>
+                        <div class="col-3">
+                            <img src="{{url('/images/logo-iom.png')}}" alt="iom">
+                        </div>
+                        <div class="col-3">
+                            <img src="{{url('/images/ole777-white.png')}}" alt="ole777">
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </body>
 </html>
